344eb6f290ffd0827181d67de4b9cf30a93825c6
inerer <arshinov04@gmail.com>
2023-01-25 12:06:38 +0300

    Добавьте файлы проекта.

---
+++ TestPraktika/AllPage/EditAddPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TestPraktika.Enteties;
+
+namespace TestPraktika.AllPage
+{
+    /// <summary>
+    /// Логика взаимодействия для EditAddPage.xaml
+    /// </summary>
+    public partial class EditAddPage : Page
+    {
+        
+        
+        Product _product;
+        public EditAddPage()
+        {
+            InitializeComponent();
+            
+            _product = new Product();    
+        }
+        public EditAddPage( Product product)
+        {
+            InitializeComponent();
+            _product = product;
+
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.DataContext = _product;
+
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            if ( _product.id == 0)
+            {
+         
+                App.testPraktika.Product.Add(_product);
+               
+            }
+            App.testPraktika.SaveChanges();
+            NavigationService.GoBack();
+        }
+    }
+}
---
+++ TestPraktika/AllPage/OrderPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TestPraktika.Enteties;
+
+namespace TestPraktika.AllPage
+{
+    /// <summary>
+    /// Логика взаимодействия для OrderPage.xaml
+    /// </summary>
+    public partial class OrderPage : Page
+    {
+        Product selectProduct;
+        public OrderPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OrderDataGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            OrderDataGrid.ItemsSource = App.testPraktika.Product.ToList();
+        }
+
+        private void OrderDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selectProduct = (Product)OrderDataGrid.SelectedItem;
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (selectProduct == null)
+            {
+                MessageBox.Show("Выберите агента!", "Сообщение", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                if (MessageBox.Show("Вы действительно хотите удалить информацию?", "Сообщение", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    App.testPraktika.Product.Remove(selectProduct);
+                    App.testPraktika.SaveChanges();
+                    OrderDataGrid_Loaded(null, null);
+                }
+            }
+
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new EditAddPage());
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (selectProduct == null)
+            {
+                MessageBox.Show("Выберите агента!", "Сообщение", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                NavigationService.Navigate(new EditAddPage(selectProduct));
+            }
+
+        }
+    }
+}
